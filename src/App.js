747c95c66297d8d10651f62c42511fcec4ff412a
Comrench <Comrench@gmail.com>
2018-09-11 22:38:16 -0400

    fixed secs hand

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Clock extends React.Component {
+class Hour extends React.Component {
   render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hour_x2: 0,
+      hour_y2: 0,
+      min_x2: 0,
+      min_y2: 0,
+      sec_x2: 0,
+      sec_y2: 0
+    }
+  }
+  render() {
+
     return (
-      <svg height="500" width="500">
-        <circle cx="50%" cy="50%" r="40%" stroke="black" stroke-width="3" fill="none" />
-        <text x="50%" y="20%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">12</text>
-        <text x="65%" y="25%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">1</text>
-        <text x="75%" y="35%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">2</text>
-        <text x="80%" y="50%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">3</text>
-        <text x="75%" y="65%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">4</text>
-        <text x="65%" y="75%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">5</text>        
-        <text x="50%" y="80%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">6</text>
-        <text x="35%" y="75%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">7</text>        
-        <text x="25%" y="65%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">8</text>
-        <text x="20%" y="50%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">9</text>
-        <text x="25%" y="35%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">10</text>
-        <text x="35%" y="25%" text-anchor="middle" fill="black" font-size="2em" font-family="Arial" dy=".3em">11</text>
+      <div>
+        <svg height="500" width="500">
+          <circle cx="50%" cy="50%" r="40%" stroke="black" strokeWidth="10" fill="none" />
+          <text x="50%" y="20%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">12</text>
+          <text x="65%" y="25%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">1</text>
+          <text x="75%" y="35%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">2</text>
+          <text x="80%" y="50%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">3</text>
+          <text x="75%" y="65%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">4</text>
+          <text x="65%" y="75%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">5</text>
+          <text x="50%" y="80%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">6</text>
+          <text x="35%" y="75%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">7</text>
+          <text x="25%" y="65%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">8</text>
+          <text x="20%" y="50%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">9</text>
+          <text x="25%" y="35%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">10</text>
+          <text x="35%" y="25%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">11</text>
 
-        <circle cx="50%" cy="50%" r="1%" stroke="black" strokeWidth="1" />
+          <circle cx="50%" cy="50%" r="1%" stroke="black" strokeWidth="1" />
 
-        <line x1="50%" y1="50%" x2="70%" y2="70%" stroke="black" strokeWidth="4" />
-        <line x1="50%" y1="50%" x2="60%" y2="70%" stroke="black" strokeWidth="4" />
-        <line x1="50%" y1="50%" x2="50%" y2="30%" stroke="black" strokeWidth="4" />
-        
-      </svg>
+          <line x1="50%" y1="50%" x2={this.state.hour_x2} y2={this.state.hour_y2} stroke="black" strokeWidth="4" />
+          <line x1="50%" y1="50%" x2={this.state.min_x2} y2={this.state.min_y2} stroke="black" strokeWidth="4" />
+          <line x1="50%" y1="50%" x2={this.state.sec_x2} y2={this.state.sec_y2} stroke="black" strokeWidth="4" />
+        </svg>
+
+        <Hour />
+      </div>
     );
   }
 }
 
 class App extends React.Component {
   render() {
+    // console.log(this.props.date);
     return (
-      <Clock />
+      // <Clock />
+      <h1>{this.props.date}</h1>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class WallClock extends React.Component {
+    render() {
+        const time = this.props.time.split(":");
+        const hour = time[0];
+        const min = time[1];
+        const sec = time[2].split(" ")[0];
+        const secDegrees = sec/10 ;
+        var x2= 250+Math.sin(secDegrees) * 100;
+        var y2= 250-Math.cos(secDegrees) * 100;
+        console.log(hour+", "+min+", "+sec+", "+x2+", "+y2+", "+secDegrees+", "+Math.sin(secDegrees)+", "+Math.cos(secDegrees));
+      return (
+        <div>
+          <svg height="500" width="500">
+            <circle cx="50%" cy="50%" r="40%" stroke="black" strokeWidth="10" fill="none" />
+            <text x="50%" y="20%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">12</text>
+            <text x="65%" y="25%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">1</text>
+            <text x="75%" y="35%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">2</text>
+            <text x="80%" y="50%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">3</text>
+            <text x="75%" y="65%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">4</text>
+            <text x="65%" y="75%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">5</text>
+            <text x="50%" y="80%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">6</text>
+            <text x="35%" y="75%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">7</text>
+            <text x="25%" y="65%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">8</text>
+            <text x="20%" y="50%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">9</text>
+            <text x="25%" y="35%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">10</text>
+            <text x="35%" y="25%" textAnchor="middle" fill="black" fontSize="2em" fontFamily="Arial" dy=".3em">11</text>
+  
+            <circle cx="50%" cy="50%" r="1%" stroke="black" strokeWidth="1" />
+  
+            {/* <line x1="50%" y1="50%" x2={this.state.hour_x2} y2={this.state.hour_y2} stroke="black" strokeWidth="4" />
+            <line x1="50%" y1="50%" x2={this.state.min_x2} y2={this.state.min_y2} stroke="black" strokeWidth="4" />
+            <line x1="50%" y1="50%" x2={this.state.sec_x2} y2={this.state.sec_y2} stroke="black" strokeWidth="4" /> */}
+
+            {/* <line x1="50%" y1="50%" x2={this.state.hour_x2} y2={this.state.hour_y2} stroke="black" strokeWidth="4" /> */}
+            {/* <line x1="50%" y1="50%" x2={this.state.min_x2} y2={this.state.min_y2} stroke="black" strokeWidth="4" /> */}
+            <line x1="250" y1="250" x2={x2} y2={y2} stroke="black" strokeWidth="4" />
+          </svg>
+        </div>
+      );
+    }
+  }
+
+function Clock(props) {
+    return (
+        <WallClock 
+            time={props.date.toLocaleTimeString()}
+        />
+    //   <div>
+        // <h1>Hello, world!</h1>
+        // <h2>It is .</h2>
+    //   </div>
+    );
+  }
+  
+  function tick() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('root')
+    );
+  }
+  
+  setInterval(tick, 1000);
+
